fc56035f821472dd0de3045c0815625731f79aee
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-18 09:11:52 +0200

    add curls for patch and delete operations

--- curls.sh
+++ curls.sh
@@ -11,3 +11,7 @@ curl -X POST localhost:3000/coffees --data "{test=data}"
 curl -X POST localhost:3000/coffees -H "Content-Type: application/json" --data '{"name":"TestName"}'
 
 curl -v -X POST localhost:3000/coffees -H "Content-Type: application/json" --data '{"name":"TestName"}'
+
+curl -X PATCH localhost:3000/coffees/123 -H "Content-Type: application/json" --data '{"name":"TestName"}'
+
+curl -X DELETE localhost:3000/coffees/123 -H "Content-Type: application/json"

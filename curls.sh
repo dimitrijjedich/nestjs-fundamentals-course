b767ccf172626a26ae877631ea2018f916d5a90a
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-18 15:39:45 +0200

    add curls for implemented crud operations

--- curls.sh
+++ curls.sh
@@ -17,3 +17,8 @@ curl -X PATCH localhost:3000/coffees/123 -H "Content-Type: application/json" --d
 curl -X DELETE localhost:3000/coffees/123 -H "Content-Type: application/json"
 
 curl "localhost:3000/coffees/flavors?limit=10&offset=5"
+
+curl localhost:3000/coffees/flavors
+curl localhost:3000/coffees/1
+curl -X POST localhost:3000/coffees -H "Content-Type: application/json" --data '{"id":2, "name":"Testname", "brand":"Testbrand", "flavors":["flavor1", "flavor2"]}'
+curl -X DELETE localhost:3000/coffees/2 -H "Content-Type: application/json"

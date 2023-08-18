b2b5a1ec21e2e7a0a3d51d746b27dc9b2127f41b
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-18 09:34:26 +0200

    add example entry to coffees service coffees property

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -3,5 +3,12 @@ import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [];
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
 }

5873ce7643e12f4fa8e87e891f8a26fac5593e1e
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-18 06:58:16 +0200

    update findOne parameter handling to extract id from parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,7 +8,7 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param() params) {
-    return `This action returns #${params.id} coffee`;
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
   }
 }

2df6a0f9c59a00bb3fb4768bebac9fc8fe4ab492
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-18 16:18:29 +0200

    create and implement update-coffee Dto
    nest generate class coffees/dto/update-coffee.dto --no-spec
    CREATE src/coffees/dto/update-coffee.dto/update-coffee.dto.ts (32 bytes)
    
    -> Copy over the properties from create-coffee Dto and make them optional
    -> Replace body with update-coffee Dto in update method of coffees controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -30,8 +31,8 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return this.coffeesService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')

217bd3f9308033b19baed3598c30015fa985b5cf
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-18 09:33:42 +0200

    cleanup coffees service

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import {Coffee} from "./entities/coffee.entity";
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-    private coffees:Coffee[] = [];
+  private coffees: Coffee[] = [];
 }

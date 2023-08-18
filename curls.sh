476a7c53b282d20fe404c543d0971aa26202ecdb
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-18 07:09:23 +0200

    add curl POST to /coffees with data containing name

--- curls.sh
+++ curls.sh
@@ -7,3 +7,5 @@ curl localhost:3000/coffees/flavors
 curl localhost:3000/coffees/123
 
 curl -X POST localhost:3000/coffees --data "{test=data}"
+
+curl -X POST localhost:3000/coffees -H "Content-Type: application/json" --data '{"name":"TestName"}'

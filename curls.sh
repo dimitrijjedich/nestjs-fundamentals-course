cccc1a2f25d1543f358c05932ecf50f8e1253209
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-18 08:56:59 +0200

    add verbose curl POST to /coffees

--- curls.sh
+++ curls.sh
@@ -9,3 +9,5 @@ curl localhost:3000/coffees/123
 curl -X POST localhost:3000/coffees --data "{test=data}"
 
 curl -X POST localhost:3000/coffees -H "Content-Type: application/json" --data '{"name":"TestName"}'
+
+curl -v -X POST localhost:3000/coffees -H "Content-Type: application/json" --data '{"name":"TestName"}'

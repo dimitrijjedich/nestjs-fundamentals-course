59c66a043a9cb0398a180d68dafc6f9714ce2811
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-19 21:08:20 +0200

    add properties and type orm annotations to flavors entity

--- src/coffees/entities/flavor.entity.ts
+++ src/coffees/entities/flavor.entity.ts
@@ -1,1 +1,10 @@
-export class Flavor {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Flavor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}

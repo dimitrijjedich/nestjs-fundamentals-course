598af21e40a164713f62edb6a2619833ef163c83
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-19 23:09:54 +0200

    add properties and type orm decorators to event entity

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,1 +1,15 @@
-export class Event {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  name: string;
+
+  @Column('json')
+  payload: Record<string, any>;
+}

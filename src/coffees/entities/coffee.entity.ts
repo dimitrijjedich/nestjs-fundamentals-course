35fa1cf10e7ead429bb6dc8d0a3b1cd21161af93
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-19 09:14:21 +0200

    add decorators to properties of coffee entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,9 +1,16 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('coffee')
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column()
   flavors: string[];
 }

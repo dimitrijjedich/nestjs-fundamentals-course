57c14b571d7783ae7ba1b23afe97ba17e1680b26
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-19 08:26:47 +0200

    import type orm module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, TypeOrmModule],
   controllers: [AppController],
   providers: [AppService],
 })

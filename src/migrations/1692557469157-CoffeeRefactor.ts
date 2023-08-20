09adb46f361153265d60e4fcda477e0fcd72e6fa
dimitrijjedich <dimitrij@startup-werk.de>
2023-08-20 21:07:12 +0200

    extend coffee refactor migration
    to execute run "npx typeorm migration:run"

--- src/migrations/1692557469157-CoffeeRefactor.ts
+++ src/migrations/1692557469157-CoffeeRefactor.ts
@@ -1,7 +1,15 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CoffeeRefactor1692557469157 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "coffee" RENAME COLUMN "name" to "title"`,
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "coffee" RENAME COLUMN "title" to "name"`,
+    );
+  }
 }
